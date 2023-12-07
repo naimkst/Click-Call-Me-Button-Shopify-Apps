32bb56da6c9233ac894017ae737330e9decb58e9
naimkst <naimkst@gmail.com>
2023-12-07 17:41:43 +0600

    update with the ui design

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -284,3 +284,4 @@ export default function Index() {
     </Page>
   );
 }
+
---
+++ extensions/call-me-button/assets/custom.js
@@ -0,0 +1,10 @@
+function callMeButtonShow() {
+  document.getElementById("showPopup").style.display = "block";
+  document.getElementById("showButton").style.display = "none";
+  document.getElementById("closeButton").style.display = "block";
+}
+function callMeButtonHide() {
+  document.getElementById("showPopup").style.display = "none";
+  document.getElementById("closeButton").style.display = "none";
+  document.getElementById("showButton").style.display = "block";
+}
