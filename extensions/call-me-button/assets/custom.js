5b49273bc3911b039d998fc9523b60232be6d980
naimkst <naimkst@gmail.com>
2023-12-19 17:20:59 +0600

    Update with port

--- extensions/call-me-button/assets/custom.js
+++ extensions/call-me-button/assets/custom.js
@@ -1,12 +1,16 @@
 function callMeButtonShow() {
-  document.getElementById("showPopup").style.display = "block";
-  document.getElementById("showPopup").style.opacity = "1";
+  // document.getElementById("showPopup").style.display = "block";
+  // document.getElementById("showPopup").style.opacity = "1";
+  var element = document.getElementById("showPopup");
+  element.classList.add("showPopup");
   document.getElementById("showButton").style.display = "none";
   document.getElementById("closeButton").style.display = "block";
 }
 function callMeButtonHide() {
-  document.getElementById("showPopup").style.opacity = "0";
-  document.getElementById("showPopup").style.display = "none";
+  // document.getElementById("showPopup").style.opacity = "0";
+  // document.getElementById("showPopup").style.display = "none";
+  var element = document.getElementById("showPopup");
+  element.classList.remove("showPopup");
   document.getElementById("closeButton").style.display = "none";
   document.getElementById("showButton").style.display = "block";
 }
